d76c40dd2fa8c33bdfb363e6cbb50731c8d16fcf
mktwohy <69822801+mktwohy@users.noreply.github.com>
2022-08-29 00:10:32 -0500

    add stubs for WorkManager

---
+++ app/src/main/java/com/example/toolnotifier/BackgroundApplication.kt
@@ -0,0 +1,10 @@
+package com.example.toolnotifier
+
+import android.app.Application
+
+class BackgroundApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+    }
+}
